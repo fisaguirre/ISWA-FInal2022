fedd59fd8b5531e9143950e32fa83623bd5c6e50
finunez <fernando.isaguirre.13@gmail.com>
2022-08-07 20:16:33 -0300

    Initial version of jhipster generated by generator-jhipster@7.8.1

---
+++ src/main/webapp/app/entities/customer/customer.model.ts
@@ -0,0 +1,25 @@
+import { IAddress } from 'app/entities/address/address.model';
+
+export interface ICustomer {
+  id?: number;
+  firstName?: string | null;
+  lastName?: string | null;
+  email?: string | null;
+  telephone?: string | null;
+  addresses?: IAddress[] | null;
+}
+
+export class Customer implements ICustomer {
+  constructor(
+    public id?: number,
+    public firstName?: string | null,
+    public lastName?: string | null,
+    public email?: string | null,
+    public telephone?: string | null,
+    public addresses?: IAddress[] | null
+  ) {}
+}
+
+export function getCustomerIdentifier(customer: ICustomer): number | undefined {
+  return customer.id;
+}
